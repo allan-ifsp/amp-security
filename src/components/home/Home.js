afc8af2c5002c71ee33a9ce74efd32cf2043333c
allan-ifsp <86083987+allan-ifsp@users.noreply.github.com>
2023-03-18 21:12:27 -0300

    comentariozin

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,12 +8,8 @@ import { Formcontato } from "./formcontato/Formcontato";
 export function Home(){
     return(
         <div> 
-            <Carousel />
-            <Jumbotron />
             
-            <Formcontato />
-            <Quadrado />
         </div> 
     )
-    //COMENTARIO PRA VER O MERGE
+    //asdasdas awdsssssssssssssss asdasdasad O asdasdasdasasdasdas
 }
