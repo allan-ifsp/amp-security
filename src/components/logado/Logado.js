61a411705c99dfab14f8d98b78f20835bd2ebbbd
allan-ifsp <86083987+allan-ifsp@users.noreply.github.com>
2023-05-01 10:53:04 -0300

    container p camera

--- src/components/logado/Logado.js
+++ src/components/logado/Logado.js
@@ -1,13 +1,14 @@
 import Footer from "../footer/Footer";
 import {NavLogado} from "./NavLogado";
 import "../header/logado.css";
+import {Camera} from "../camera/Camera";
 
 export function Logado(){
     return(
         <div>
 
             <NavLogado />
-            
+            <Camera />
         </div>
     )
 }
