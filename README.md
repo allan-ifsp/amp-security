26f752a478109c0f80059021b3e37257f862dbbb
allan-ifsp <86083987+allan-ifsp@users.noreply.github.com>
2023-05-13 20:56:48 -0300

    xD

--- README.md
+++
@@ -1,3 +0,0 @@
-Para rodar o programa react, entre na pasta do projeto e digite as seguintes linhas no terminal
-- npm install
-- npm start
--- src/components/camera/Camera.js
+++
@@ -1,7 +0,0 @@
-export function Camera(){
-    return (
-        <div>
-            <img src="./imagens/camera1.jpg" className="d-block vh-100 w-100" alt="..." />
-        </div>
-    )
-}
